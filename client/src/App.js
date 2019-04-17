5fd632d34570b3e7a4753ad5a53f5779b80c173a
kauvia <mkhw1989@gmail.com>
2019-04-17 12:26:10 +0800

    first init

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import GameScreen from './components/game/gamescreen';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      App
+      <GameScreen/>
+      </div>
+    );
+  }
+}
+
+export default App;
